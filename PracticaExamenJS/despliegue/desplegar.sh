5568e3a7b2911f2e5b370dd1212ca2e3871ad35b
sergio <sergiovanacloig97@gmail.com>
2018-02-12 09:35:17 +0100

    Arreglando deplegar

--- PracticaExamenJS/despliegue/desplegar.sh
+++ PracticaExamenJS/despliegue/desplegar.sh
@@ -8,5 +8,5 @@ cd ..
 cd ..
 mkdir mysql
 docker run -d -p 80:80 --net=red_docker --ip 192.168.1.111 --name mierder -v /opt/mierder/html:/var/www/html nimmis/apache-php7
-docker run -d -p 3306:3306 --net=red_docker --ip 192.168.1.121 --name mysql_mierder -v /opt/mierder/sql/mysql -e MYSQL_ROOT_PASSWORD=root -v /opt/mierder/database:/database mysql
+docker run -d -p 3306:3306 --net=red_docker --ip 192.168.1.121 --name mysql_mierder -v /opt/mierder/sql/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root -v /opt/mierder/database:/database mysql
 docker exec -t mysql_mierder /bin/bash /database/import.sh
